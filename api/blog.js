5ceeb0eb41034902d577efbfe446f292393ef71d
DigionTop <digiontop.agency@gmail.com>
2026-06-16 15:25:16 +0530

    debug: expose error detail in API 500 responses (temporary)

--- api/blog.js
+++ api/blog.js
@@ -151,6 +151,6 @@ export default async function handler(req, res) {
 
     return res.status(405).json({ error: 'Method not allowed' });
   } catch (err) {
-    return res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).json({ error: 'Internal server error', detail: String(err?.message || err) });
   }
 }
